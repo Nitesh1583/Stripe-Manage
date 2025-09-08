a2ad3815d681ca520f478f0a7b3fa524f308ab34
Nitesh Kumar <79831502+Nitesh1583@users.noreply.github.com>
2025-09-08 11:25:16 +0530

    Update New changes in App file

--- app/models/invoices.server.tsx
+++ app/models/invoices.server.tsx
@@ -1,5 +1,6 @@
 import { Stripe } from "stripe";
 
+// Fetch All Invoices 
 export async function fetchStripeInvoices(userInfo) {
   try {
     const stripe = new Stripe(userInfo.stripeSecretKey, { apiVersion: "2023-10-16" });
@@ -28,6 +29,7 @@ export async function fetchStripeInvoices(userInfo) {
   }
 }
 
+// fetch Invoices by search filter
 export async function fetchSearchStripeInvoices(searchValue, userInfo) {
   try {
     const stripe = new Stripe(userInfo.stripeSecretKey, { apiVersion: "2023-10-16" });
@@ -43,5 +45,35 @@ export async function fetchSearchStripeInvoices(searchValue, userInfo) {
   } catch (error) {
     console.error("Error searching invoices:", error);
     return { stripeInvoices: [], message: "Search failed", error, isError: true };
+  }
+}
+
+
+// fetch recent invoices on dashboard page
+export async function fetchStripeRecentInvoices(userInfo) {
+  try {
+    const stripe = new Stripe(userInfo.stripeSecretKey, { apiVersion: "2023-10-16" });
+
+    // Fetch all invoices (no customer filter)
+    const recentInvoices = await stripe.invoices.list({ limit: 5 });
+
+    const invoiceData = recentInvoices.data.map((inv) => ({
+      id: inv.id,
+      customerName: inv.customer_name,
+      amount: (inv.amount_due / 100).toFixed(2),
+      currency: inv.currency?.toUpperCase() || "USD",
+      status: inv.status,
+      created: new Date(inv.created * 1000).toLocaleDateString(),
+    }));
+
+    return { recentInvoices: invoiceData, isError: false };
+  } catch (error) {
+    console.error("Error fetching invoices:", error);
+    return {
+      recentInvoices: [],
+      message: "Unable to fetch invoices",
+      error,
+      isError: true,
+    };
   }
 }
