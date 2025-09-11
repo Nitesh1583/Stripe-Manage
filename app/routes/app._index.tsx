8aebaaa23e550ee73b82d00aaa90bdeca31b5b1a
Nitesh Kumar <79831502+Nitesh1583@users.noreply.github.com>
2025-09-11 11:37:34 +0530

    Update condition index file

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -393,7 +393,7 @@ export default function Index() {
                 <Text as="h2" variant="headingMd">
                   Recent Invoices
                 </Text>
-                <InvoicesPlaceholder height="100%" recentInvoices = {recentInvoices}/>
+                <InvoicesPlaceholder height="100%" recentInvoices = {recentInvoices} premiumUser={premiumUser} //>
               </Grid.Cell>
             </Grid>
           </LegacyCard>
@@ -754,7 +754,7 @@ const InvoicesPlaceholder = ({
   planStatus,
   premiumUser
 }) => {
-  const numericPremiumUser = premiumUser; // ✅ always number
+  const numericPremiumUser = Number(premiumUser); 
 
   return (
     <div
